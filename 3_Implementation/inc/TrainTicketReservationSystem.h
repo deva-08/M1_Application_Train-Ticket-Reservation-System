d4dc9dcb702772524e46f8c60d60d0e42e7d6e3c
deva-08 <94280572+deva-08@users.noreply.github.com>
2021-11-23 20:59:40 +0530

    Update TrainTicketReservationSystem.h

--- 3_Implementation/inc/TrainTicketReservationSystem.h
+++ 3_Implementation/inc/TrainTicketReservationSystem.h
@@ -1,1 +1,33 @@
+/**********************************************PREPROCESSORS**********************************************************/
+//Train Reservation System - BROUGHT TO YOU BY : code-projects.org
+//Working on basic structure
+//including all libraries for now
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#include<string.h>
+
+/*******************************************GLOBAL VARIABLES**********************************************************/
+//ALl the globle variables and the composite data types will be declared here
+typedef struct{
+	char name[50];
+	int train_num;
+	int num_of_seats;
+}pd;
+
+
+
+
+/*******************************************FUNCTION PROTOTYPE**********************************************************/
+//function prototypes to be used
+void reservation(void);							//main reservation function
+void viewdetails(void);							//view details of all the trains
+void cancel(void);								
+void printticket(char name[],int,int,float);	//print ticket 
+void specifictrain(int);						//print data related to specific train
+float charge(int,int);							//charge automatically w.r.t number of seats and train
+void login();
+
+
+/******************************************FUNCTION DECLARATION**********************************************************/
 
